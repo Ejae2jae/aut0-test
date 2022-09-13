8796fd391f1ae41b348917601eadd4b5cceb9744
ejae2jae <dreary3@naver.com>
2022-09-13 18:29:33 +0900

    test

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# ejae
-
-https://wbluke.tistory.com/39?category=418851
-해당 블로그를 참고했습니다. 
+aut0-test
--- playground-core/src/main/java/com/wbluke/playground/core/domain/article/ArticleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.wbluke.playground.core.domain.article;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
